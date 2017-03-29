1ec07e4b348c673d8b0ea92bf3fb232e1b3bdd99
Yuvraj3079 <yuvrajsingh.ys736@gmail.com>
2017-03-29 14:52:04 +0530

    Create helloword.java

---
+++ helloword.java
@@ -0,0 +1,13 @@
+public class helloworld
+{
+  public static void main(Stirng agrs[])
+  {
+  System.out.println("hii there");
+  System.out.println("whats up");
+ // System.out.println("");
+  
+  
+  }
+  
+
+}
